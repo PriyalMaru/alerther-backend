c63333384b93e4cfa07df526b02bb3d75384a8b8
Priyal Maru <marupriyal05@gmail.com>
2026-04-20 12:46:33 +0530

    Initial backend commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/alert')
+def alert():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    emergency_type = request.args.get('type', 'Emergency')
+
+    print("🚨 ALERT RECEIVED!")
+    print("Type:", emergency_type)
+    print("Location:", lat + ", " + lon)
+    print("Google Maps: https://maps.google.com/?q=" + lat + "," + lon)
+
+    return "Alert Triggered"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
